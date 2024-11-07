4622a3cdb0b224832bb5cd5c7846bc1969ef521c
klynxlx <katunka.ua@gmail.com>
2024-11-07 19:07:15 +0200

    update

---
+++ js/lab5.js
@@ -0,0 +1,59 @@
+function showDetails5() {
+    var outputDiv = document.getElementById("output");
+    outputDiv.innerHTML = `
+<p><strong>Тема:</strong> ФУНКЦІОНАЛЬНЕ ЗАСТОСУВАННЯ JAVASCRIPT У HTML-ДОКУМЕНТІ. 
+ВИКОРИСТАННЯ МАСИВІВ У JS-СЦЕНАРІЯХ. РЕАЛІЗАЦІЯ ПРОГРАМ ЗАСОВАМИ МОВИ JAVASCRIPT</p>
+<p><strong>Мета:</strong> ⎯	придбати практичні навички роботи з конструкціями мови JS, масивами та фугкціями у js-сценаріях.  Реалізація програм засовами мови JAVASCRIPT</p>
+<p><strong>Місце розташування:</strong></p>
+<ul>
+    <li>Посилання на репозиторій власного WEB-застосунку: 
+        <a href="https://github.com/klynxlx/linnyk_lab2">https://github.com/klynxlx/linnyk_lab2</a>
+    </li>
+    <li>Посилання на живу сторінку власного WEB-застосунку: 
+        <a href="https://klynxlx.github.io/linnyk_lab2/">https://klynxlx.github.io/linnyk_lab2/</a>
+    </li>
+    <li>Посилання на репозиторій звітного HTML документу: 
+        <a href="https://github.com/klynxlx/zvit-document">https://github.com/klynxlx/zvit-document</a>
+    </li>
+    <li>Посилання на живу сторінку звітного HTML документу: 
+        <a href="https://klynxlx.github.io/zvit-document/">https://klynxlx.github.io/zvit-document/</a>
+    </li>
+</ul>`
+;}
+
+function Task1() {
+    var outputDiv = document.getElementById("output");
+    outputDiv.innerHTML = `
+            <img src="./images/task1.jpg" alt="Placeholder Image" width="700">`
+
+        ;
+    }
+function Task2() {
+    var outputDiv = document.getElementById("output");
+    outputDiv.innerHTML = `
+            <img src="./images/task2.jpg" alt="Placeholder Image" width="700">`
+            
+        ;
+    }
+    function Task3() {
+        var outputDiv = document.getElementById("output");
+        outputDiv.innerHTML = `
+                <img src="./images/task3.jpg" alt="Placeholder Image" width="700">`
+                
+            ;
+        }
+        function Task4() {
+            var outputDiv = document.getElementById("output");
+            outputDiv.innerHTML = `
+                    <img src="./images/task4.1.jpg" alt="Placeholder Image" width="700">
+                    <img src="./images/task4.2.jpg" alt="Placeholder Image" width="700">`
+                ;
+            }
+
+            function showConclusion5() {
+                var outputDiv = document.getElementById("output");
+                var conclusion = `
+                    <p><strong>Висновок:</strong> Під час виконання ЛР №2 я придбала практичні навички роботи з конструкціями мови JS, масивами та функціями у js-сценаріях. Також реалізувала програми засовами мови JAVASCRIPT</p>`
+                ;
+                outputDiv.innerHTML = conclusion;
+            }      
