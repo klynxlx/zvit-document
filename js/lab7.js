4dcd42e721544def19b82cd0e1a44c441f456aa8
klynxlx <katunka.ua@gmail.com>
2024-12-03 14:44:37 +0200

    update

---
+++ js/lab7.js
@@ -0,0 +1,88 @@
+function showDetails7() {
+    var outputDiv = document.getElementById("output");
+    outputDiv.innerHTML = `
+<p><strong>Тема:</strong> КЛАСИ. ОБ'ЄКТНА МОДЕЛЬ ДОКУМЕНТА (DOM). ПОДІЇ. ОБ'ЄКТ ПОДІЇ. ДЕЛЕГУВАННЯ ПОДІЙ. </p>
+<p><strong>Мета:</strong> придбати практичні навички роботи з об'єктами. Методи об'єкта.. Callback. Стрілочні функції. Стрілочні функції як колбеки.
+.  Реалізація програм засовами мови JAVASCRIPT</p>
+<p><strong>Місце розташування:</strong></p>
+<ul>
+    <li>Посилання на репозиторій власного WEB-застосунку: 
+        <a href="https://github.com/klynxlx/linnyk_lab2">https://github.com/klynxlx/linnyk_lab2</a>
+    </li>
+    <li>Посилання на живу сторінку власного WEB-застосунку: 
+        <a href="https://klynxlx.github.io/linnyk_lab2/">https://klynxlx.github.io/linnyk_lab2/</a>
+    </li>
+    <li>Посилання на репозиторій звітного HTML документу: 
+        <a href="https://github.com/klynxlx/zvit-document">https://github.com/klynxlx/zvit-document</a>
+    </li>
+    <li>Посилання на живу сторінку звітного HTML документу: 
+        <a href="https://klynxlx.github.io/zvit-document/">https://klynxlx.github.io/zvit-document/</a>
+    </li>
+</ul>`
+;}
+
+function Task7_2() {
+    var outputDiv = document.getElementById("output");
+    outputDiv.innerHTML = `
+            <img src="./lab7.tasks/images7/task2.jpg" alt="Placeholder Image" width="700">
+            <img src="./lab7.tasks/images7/taask2.jpg" alt="Placeholder Image" width="700">`
+
+        ;
+    }
+function Task7_4() {
+    var outputDiv = document.getElementById("output");
+    outputDiv.innerHTML = `
+            <img src="./lab7.tasks/images7/task4.jpg" alt="Placeholder Image" width="700">
+            <img src="./lab7.tasks/images7/taask4.jpg" alt="Placeholder Image" width="700">`
+            
+        ;
+    }
+    function Task7_6() {
+        var outputDiv = document.getElementById("output");
+        outputDiv.innerHTML = `
+                <img src="./lab7.tasks/images7/task6.jpg" alt="Placeholder Image" width="700">
+                <img src="./lab7.tasks/images7/taask6.jpg" alt="Placeholder Image" width="700">`
+                
+            ;
+        }
+        function Task7_7() {
+            var outputDiv = document.getElementById("output");
+            outputDiv.innerHTML = `
+                    <img src="./lab7.tasks/images7/task7.jpg" alt="Placeholder Image" width="700">
+                    <img src="./lab7.tasks/images7/taask7.jpg" alt="Placeholder Image" width="700">
+                   `
+                ;
+            }
+        function Task7_8() {
+            var outputDiv = document.getElementById("output");
+            outputDiv.innerHTML = `
+                    <img src="./lab7.tasks/images7/task8_2.jpg" alt="Placeholder Image" width="700">
+                    <img src="./lab7.tasks/images7/task8_1.jpg" alt="Placeholder Image" width="700">
+                    <img src="./lab7.tasks/images7/taask8.jpg" alt="Placeholder Image" width="700">`
+                ;
+            }
+            function Task7_9() {
+                var outputDiv = document.getElementById("output");
+                outputDiv.innerHTML = `
+                        <img src="./lab7.tasks/images7/task9.jpg" alt="Placeholder Image" width="700">
+                    
+                        <img src="./lab7.tasks/images7/taask9.jpg" alt="Placeholder Image" width="700">`
+                    ;
+                }
+                function Task7_10() {
+                    var outputDiv = document.getElementById("output");
+                    outputDiv.innerHTML = `
+                            <img src="./lab7.tasks/images7/task10.jpg" alt="Placeholder Image" width="700">
+                        
+                            <img src="./lab7.tasks/images7/taask10.jpg" alt="Placeholder Image" width="700">`
+                        ;
+                    }
+            function showConclusion7() {
+                var outputDiv = document.getElementById("output");
+                var conclusion = `
+                    <p><strong>Висновок:</strong> Під час виконання ЛР №7 я придбала практичні навички роботи з об'єктами. Методи об'єкта.. Callback. Стрілочні функції. Стрілочні функції як колбеки.
+.  Реалізація програм засовами мови JAVASCRIPT
+</p>`
+                ;
+                outputDiv.innerHTML = conclusion;
+            }  
