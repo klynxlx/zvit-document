6cb6353c86b184a59d2d52d0b09c8dacafc91f25
klynxlx <katunka.ua@gmail.com>
2024-12-16 14:09:40 +0200

    update

---
+++ js/lab8.js
@@ -0,0 +1,48 @@
+function showDetails8() {
+    var outputDiv = document.getElementById("output");
+    outputDiv.innerHTML = `
+<p><strong>Тема:</strong> WEB-СХОВИЩЕ. WEB STORAGE API. МОДУЛЬНОСТЬ  КОДУ. СТВОРЕННЯ МОДАЛЬНОГО ВІКНА. ДЕЛЕГУВАННЯ ПОДІЙ. </p>
+<p><strong>Мета:</strong> придбати практичні навички роботи створення модального вікна; повторити процес створення макету, зокрема створення макету галереї довільних зображень; закріпити навички роботи з делегуванням подій.</p>
+<p><strong>Місце розташування:</strong></p>
+<ul>
+    <li>Посилання на репозиторій лабораторної роботи №8: 
+        <a href="https://github.com/klynxlx/lab8">https://github.com/klynxlx/lab8</a>
+    </li>
+    <li>Посилання на живу сторінку лабораторної роботи №8: 
+        <a href="https://klynxlx.github.io/lab8/">https://klynxlx.github.io/lab8/</a>
+    </li>
+    <li>Посилання на репозиторій звітного HTML документу: 
+        <a href="https://github.com/klynxlx/zvit-document">https://github.com/klynxlx/zvit-document</a>
+    </li>
+    <li>Посилання на живу сторінку звітного HTML документу: 
+        <a href="https://klynxlx.github.io/zvit-document/">https://klynxlx.github.io/zvit-document/</a>
+    </li>
+</ul>`
+;}
+
+function Task8_1() {
+    var outputDiv = document.getElementById("output");
+    outputDiv.innerHTML = `
+            <img src="./images/8_1.jpg" alt="Placeholder Image" width="700">
+            <img src="./images/code8_1.jpg" alt="Placeholder Image" width="700">
+            <img src="./images/code8_11.jpg" alt="Placeholder Image" width="700">`
+
+        ;
+    }
+function Task8_2() {
+    var outputDiv = document.getElementById("output");
+    outputDiv.innerHTML = `
+             <img src="./images/8_2.jpg" alt="Placeholder Image" width="700">
+            <img src="./images/code8_2.jpg" alt="Placeholder Image" width="700">`
+            
+        ;
+    }
+
+    function showConclusion8() {
+        var outputDiv = document.getElementById("output");
+        var conclusion = `
+            <p><strong>Висновок:</strong> Під час виконання ЛР №7 я придбала практичні навички роботистворення модального вікна; повторити процес створення макету, зокрема створення макету галереї довільних зображень; закріпити навички роботи з делегуванням подій.
+</p>`
+        ;
+        outputDiv.innerHTML = conclusion;
+    }  
